2b5baf913b701dc522e2a77a6e8ed165b47a48e8
shenyunjie <shenyunjie@u51.com>
2018-03-21 12:39:24 +0800

    commit code: ys-pg-micro-router@1.0.2

---
+++ app.js
@@ -0,0 +1,67 @@
+const Router = require('ys-middleware-router');
+const fs = require('fs');
+const path = require('path');
+const util = require('ys-utils');
+
+module.exports = (app, configs) => {
+  app.on('serverWillStart', () => {
+    const routerPath = path.resolve(app.options.baseDir, 'app/router');
+    if (!fs.existsSync(routerPath)) {
+      throw new Error(`router of '${routerPath}' must be exists`);
+    }
+    app.router = loadRoutesModules(routerPath, app, configs);
+    app.console.log(`  - [${app.pid}]`, '[`ys-pg-koa-router`:app] Routers Loaded: ' + routerPath);
+    app.micro.use(app.router.routes());
+  });
+}
+
+function loadRoutesModules(dir, inject, configs) {
+  return load(dir);
+
+  function load(dir, name, route) {
+    if (!fs.existsSync(dir)) return;
+    const { files, dirs } = collectDirs(dir);
+    const indexRouter = files['index.js'];
+    const router = parse(inject, indexRouter, configs);
+    for (const i in files) {
+      if ('index.js' === i) continue;
+      const _router = parse(inject, files[i], configs);
+      router.use('/' + replacePrefix(i), _router.routes(), _router.allowedMethods());
+    }
+    for (const j in dirs) load(dirs[j], j, router);
+    if (name) route.use('/' + name, router.routes(), router.allowedMethods());
+    return router;
+  }
+}
+
+function collectDirs(dir) {
+  const files = fs.readdirSync(dir);
+  const _files = {};
+  const _dirs = {};
+
+  files.forEach(file => {
+    const _path = path.resolve(dir, file);
+    if (fs.statSync(_path).isDirectory()) {
+      _dirs[file] = _path;
+    } else {
+      _files[file] = _path;
+    }
+  });
+
+  return {
+    files: _files,
+    dirs: _dirs
+  }
+}
+
+function parse(inject, file, configs) {
+  if (!file) return new Router(configs);
+  const modal = util.file.load(file);
+  const router = new Router(configs);
+  modal(inject, router);
+  return router;
+}
+
+function replacePrefix(d) {
+  return d.replace(/\.js$/, '');
+}
---
+++ ys.command.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const fs = require('fs-extra');
+const { spawnSync } = require('child_process');
+const dbo = require('ys-dbo');
+
+exports.use = `{ enable: true, package: 'ys-pg-micro-router' }`;
+exports.common = `{}`;
+
+exports.installer = async ({ cwd, log }) => {
+  const routerDir = path.resolve(cwd, 'app', 'router');
+  const indexFilePath = path.resolve(routerDir, 'index.js');
+  if (!fs.existsSync(routerDir)) {
+    fs.mkdirSync(routerDir);
+  }
+  if (fs.existsSync(indexFilePath)) {
+    throw new Error(`file '${indexFilePath}' is already exists.`);
+  }
+  const data = `module.exports = (app, router) => {
+  router.receive('/', app.controller.index);
+}`;
+  fs.writeFileSync(indexFilePath, data, 'utf8');
+  log.success(`写入初始路由文件成功 - '${indexFilePath}'`);
+}
+
+exports.uninstaller = async ({ cwd }) => {
+  const code = spawnSync('rm', ['-rf', 'app/router'], {
+    stdio: 'inherit',
+    cwd
+  });
+  if (!code) {
+    throw new Error('Run command catch error');
+  }
+}
+
+exports.command = ({ app, log, root }) => {
+  app.command('router <name>')
+    .describe('创建一个新的路由')
+    .action(name => {
+      new dbo().until(async thread => {
+        const relativePath = path.relative(root, process.cwd());
+        const routerPath = path.resolve(root, 'app/router');
+        const isInRouterDir = relativePath.indexOf('app/router') === 0;
+        if (isInRouterDir) {
+          return exports.addFile(process.cwd(), name, log);
+        }
+        await exports.addFile(routerPath, name, log);
+      }, {
+        error(e) {
+          log.error(e.message);
+          process.exit(1);
+        }
+      });
+    });
+}
+
+exports.addFile = async (cwd, name, log) => {
+  name = name.replace(/\.js$/i, '');
+  if (!/^[a-z][a-z0-9_]*$/.test(name)) {
+    throw new Error('模块命名不规范');
+  }
+  const filePath = path.resolve(cwd, name + '.js');
+  const dir = path.dirname(filePath);
+  fs.ensureDirSync(dir);
+  if (fs.existsSync(filePath)) {
+    throw new Error(`file '${filePath}' is already exists.`);
+  }
+  const data = `module.exports = (app, router) => {}`;
+  fs.writeFileSync(filePath, data, 'utf8');
+  log.success(`写入路由文件成功 - '${filePath}'`);
+}
